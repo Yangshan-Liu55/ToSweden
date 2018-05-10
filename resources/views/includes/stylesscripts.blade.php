407b78cfa6f2f53b003f422ee01ad6da4c2ec703
Steffo <steffo.dimfelt@gmail.com>
2018-05-10 14:30:03 +0200

    Rename indexAPI.js to indexControllers.js

--- resources/views/includes/stylesscripts.blade.php
+++ resources/views/includes/stylesscripts.blade.php
@@ -7,5 +7,5 @@
        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.6/umd/popper.min.js"></script>
        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js"></script>
        <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.9/angular.min.js"></script>
-       <script src="/js/indexApi.js"></script>
+       <script src="/js/indexControllers.js"></script>
        <link rel="stylesheet" type="text/css" href="/css/index.css">
