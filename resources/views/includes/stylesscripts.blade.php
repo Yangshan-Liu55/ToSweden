bf359bce34a76c905bdbec50eefbbe2182a20880
Steffo <steffo.dimfelt@gmail.com>
2018-05-14 13:08:51 +0200

    FontAweson linked

--- resources/views/includes/stylesscripts.blade.php
+++ resources/views/includes/stylesscripts.blade.php
@@ -7,6 +7,7 @@
        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.6/umd/popper.min.js"></script>
        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js"></script>
        <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.9/angular.min.js"></script>
+       <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
        <script src="/js/indexControllers.js"></script>
        <script src="/js/jqueryFunctions.js"></script>
        <link rel="stylesheet" type="text/css" href="/css/index.css">
