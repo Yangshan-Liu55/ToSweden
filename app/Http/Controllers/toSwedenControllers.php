9ab2e2e79b975b6694ca9b6f9c20323037a9620f
Steffo <steffo.dimfelt@gmail.com>
2018-05-16 23:38:45 +0200

    Test av globala sessions

--- app/Http/Controllers/toSwedenControllers.php
+++ app/Http/Controllers/toSwedenControllers.php
@@ -1,13 +1,31 @@
 <?PHP
+
 namespace App\Http\Controllers;
+
 use Illuminate\Http\Request;
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+use Session;
+
 
 class toSwedenControllers extends Controller{
 
-    public function recommended()
+   /* public function translate($lang)
     {
-     
-    }
+        Session::put('language','en');
+    } */
+
+    public function storeSessionData(Request $request){
+        $request->session()->put('my_name','Virat Gandhi');
+        echo "Data has been added to session";
+     }
+
+     public function accessSessionData(Request $request){
+        if($request->session()->has('my_name'))
+           echo $request->session()->get('my_name');
+        else
+           echo 'No data in the session';
+     }
     
 }
 
