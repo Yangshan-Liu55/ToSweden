741eccacedab352464f7bf39d821eaf355ca6dbb
shoonne <shaon_ahmed@msn.com>
2018-06-04 14:49:33 +0200

    Added extra text about

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -33,17 +33,20 @@
         <div  align ="center">
         <div class="center"> 
                 <img style="width:100%; " src="/img/foto/swedenabout.jpg" />
+                <div align="center">
+                <a id="dArrow" href="#about" ><img class="aboutpic" src="/img/foto/scrollarrow.png" alt=""></a>    <!-- SCROLL DOWN PIL -->
+        </div>
         </div>
         </div>
     <!-- TEXT OM oss -->
-    <h1 class="text-center mt-5">Om To Sweden</h1>
+    <h1 id="about" class="text-center mt-5">Om To Sweden</h1>
         <div class="container">         
         <hr class="hr" size=1   color="#ffcc00" style="border:2 solid #ffcc00">
         <p class="px-5 darkblue-bg p-4 m-2 rounded">Vi leder alla till Sverige. To Sweden samarbetar med Svenska Olympiska kommittén och den 
             internationella olympiska kommittén för att hjälpa världens medborgare till årtusendets event.<BR><BR>
             Vi anser att en lyckad upplevelse börjar redan vid resan, därför har vi lagt mycket tid och omsorg
             för att hitta de rätta vägarna till årets olympiska spel. <BR><BR>
-                Välkommen till landet av snö och is!
+               <span class="text-center">Välkommen till landet av snö och is!</span> 
         </p>
      
         <div class="row " align="center">
@@ -64,9 +67,13 @@
     <h1 class="text-center mt-5">Historien om OS</h1>
         <div class="container">         
         <hr class="hr" size=1   color="#ffcc00" style="border:2 solid #ffcc00">
-        <p class="text-center darkblue-bg p-4 m-2 rounded">Den franske baronen och filosofen Pierre de Coubertin grundade de olympiska spelen 1894. Han var övertygad om att
-            man kan göra världen bättre med hjälp av idrott. De första olympiska spelen arrangerades i Aten 1896. Inspirationen
-            till spelen var hämtade från idrottstävlingar som hölls till guden Zeus ära i Grekland under antiken.
+        <p class="text-center darkblue-bg p-4 m-2 rounded">Den första olympiska vinterspelen hölls i Chamonix 1924. Spelen 2026 blir de 26:e i ordningen. Sverige har genom åren tagit 51 guld, 35 silver och 49 brons i vinter-OS. Det ger totalt 135 medaljer och en sjunde plats i medaljligan genom tiderna.
+            En olympisk vinteridrott utövas på snö eller is. 
+            <br><br>
+            På det olympiska programmet finns just nu 15 discipliner: alpin skidåkning, backhoppning, bob, curling, freestyle, ishockey , konståkning, längdåkning, nordisk kombination, rodel, short track, skeleton, skidskytte, skridsko och snowboard.
+            <br><br>
+            Vid OS i PyeongChang 2018 genomfördes 102 medaljgrenar under 17 tävlingsdagar. I Sydkorea fanns 93 nationer på plats och det var nästan 3 000 ackrediterade aktiva. 
+            Vid vinterspelen dessförinnan, Sotji 2014, deltog 2 873 utövare från 88 olika nationer, varav 1 714 var män och 1 159 var kvinnor.
         </p>
      
         <div class="row " align="center">
@@ -83,6 +90,65 @@
         </div>
     </div>
 <!-- Slut om OS -->
+
+<h1 id="about" class="text-center mt-5">Ett svenskt vinter-OS ska bli den inspirationskälla vi vill att OS ska vara:</h1>
+        <div class="container">         
+        <hr class="hr" size=1   color="#ffcc00" style="border:2 solid #ffcc00">
+        <p class="px-5 darkblue-bg p-4 m-2 rounded"> 
+        <strong>Samlande.</strong> 
+        <br> 
+        Själva grunden i Pierre de Coubertins olympiska tanke är dess samlande kraft. 
+        Spelen samlar de bästa idrottarna från hela världen; de tävlar tillsammans, bor tillsammans och äter tillsammans. 
+        Det är symbolen för att fredlig samexistens är möjlig över alla gränser även i turbulenta tider. "Unity in Diversity". 
+        <br><br>
+       <strong>Hållbara. </strong> 
+        <br> 
+        De olympiska och paralympiska spelen har förändrats. Den olympiska rörelsens nya strategi Agenda 2020, 
+        med dess högt ställda ambitioner om social, ekonomisk, miljömässig och demokratisk hållbarhet, 
+        passar väl in på Stockholms och Sveriges profil. 
+        Vinterspelen är en chans att visa upp ett område där vi ur flera aspekter är världsledande, 
+        samtidigt som projektet både kan kräva och skapa inspiration för ännu mer ansträngning för att befästa tätpositionen. 
+        Det är också en chans för oss att sätta en tydlig prägel på spelen och visa hur hållbarhet kan bli en integrerad konkret del av den olympiska och paralympiska tanken, 
+        ett internationellt föredöme. Vi kan skapa hållbara vinterspel som kan inspirera och samla den olympiska och paralympiska rörelsen, precis som Stockholm gjorde 1912.
+        <br><br>
+        <strong>Vitaliserande. </strong> 
+        <br>
+        OS och Paralympics ger en möjlighet till vitalisering av svensk topp-, tävlings- och ungdomsidrott. 
+        Vinterspelen blir en hävstång för att utveckla idrotten. För det krävs en rejäl satsning från alla inblandade parter: 
+        idrottsrörelsen, staden, staten och näringslivet. 
+        Med ett samlat engagemang kan OS och Paralympics ge skjuts till att fler ska träna mer, 
+        satsa hårdare och nå bättre resultat. 
+        Stödet till detta kan och behöver förbättras så att fler tjejer och killar ska ges möjligheter att satsa fullt ut och för att Sverige ska kunna behålla och utveckla den internationella konkurrensnivån. 
+        Efter spelen kommer bättre anläggningar att finnas för både tävlings-, ungdoms- och breddidrotten. 
+        Stockholm kommer att i direkt anslutning till innerstaden ha anläggningar för skridskoåkning, 
+        längdskidåkning och betydligt bättre förutsättningar för utförsåkning.
+        <br><br>
+        <strong>Inspirerande.</strong> 
+        <br>
+        Vinterspelen kan inspirera många människor till en aktiv livsstil även i storstad, även utomhus och även på vintern. 
+        Uppmärksamhet på värdet av god hälsa skapas och idrott placeras mitt i det offentliga samtalet under nästan tio års tid. 
+        Vinterspelen kommer att motivera idrottare, inte minst ungdomar, till att vilja göra mer och prova nytt. 
+        Fleråriga satsningar på nya unga ledare, ”OS i skolan”, 
+        prova på aktiviteter för barn och ungdomar samt nya idrottsformer i icke idrottstraditionella miljöer och stadsdelar ges 
+        ett sammanhang. Vinterspel på hemmaplan ger inte bara kraft åt Paralympics och paraidrottare med siktet inställt på att 
+        delta själva, utan fäster uppmärksamhet på tillgänglighet både för idrott och ur ett större samhälleligt perspektiv.
+            
+            
+        </p>
+     
+        <div class="row " align="center">
+        
+        <div class="col col-1 m-0 p-0">
+            <p><img  src="/img/Paint/squer.png" alt="">
+        </div>
+
+        <div class="col "> <hr class="yellow-bg" size="5"> </div>
+        
+        <div class="col col-1 m-0 p-0">
+            <img  src="/img/Paint/squer.png" alt=""></p>
+        </div>
+        </div>
+    </div>
 
    <br>
    <div  align ="center" class=" pt-2 pb-4">
